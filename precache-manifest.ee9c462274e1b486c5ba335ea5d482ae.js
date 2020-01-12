dd7c5af60328e2528ac50762dd83346a75207697
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-01-12 16:48:41 +0300

    Updates

---
+++ precache-manifest.ee9c462274e1b486c5ba335ea5d482ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7543200305c83ed48b836fc2d2d63f43",
+    "url": "/counter_Redux/index.html"
+  },
+  {
+    "revision": "eecf2851db67eff7bf91",
+    "url": "/counter_Redux/static/css/main.11e17efc.chunk.css"
+  },
+  {
+    "revision": "5a28b519ac8df4436f2c",
+    "url": "/counter_Redux/static/js/2.6968eb9b.chunk.js"
+  },
+  {
+    "revision": "eecf2851db67eff7bf91",
+    "url": "/counter_Redux/static/js/main.d354e0d2.chunk.js"
+  },
+  {
+    "revision": "bf713a2eb55afff7564b",
+    "url": "/counter_Redux/static/js/runtime-main.970d1434.js"
+  },
+  {
+    "revision": "91d7bec04ef67afa7d86dacd511dc313",
+    "url": "/counter_Redux/static/media/numbers.91d7bec0.jpg"
+  }
+]);
