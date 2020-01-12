460762b469fb7cb5806580a88267558f6eea4bfa
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-01-12 16:28:52 +0300

    html coding,refactoring

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import SettingsCounter from './SettingsCounter';
 import Counter from './Counter';
 import {connect} from "react-redux";
-import {onIsButtonIncAC,onIsButtonResetAC,onIsButtonSetAC,
-    onMaxValueReduxAC,onMinValueReduxAC, onResetCounterAC, 
-    onSetEnteredValueAC, onSetIncorrectValueAC,onSetReduxAC, 
-    setErrorMaxAC, setErrorMinAC, setIncrementAC, setNumberNewColorAC} from "./reducer";
+import {
+    onIsButtonIncAC, onIsButtonResetAC, onIsButtonSetAC,
+    onMaxValueReduxAC, onMinValueReduxAC, onResetCounterAC,
+    onSetEnteredValueAC, onSetIncorrectValueAC, onSetReduxAC,
+    setErrorMaxAC, setErrorMinAC, setIncrementAC, setNumberNewColorAC
+} from "./reducer";
 
 
 class App extends React.Component {
 
-    btns = {reset: "reset", inc: "inc", set: "set"}
+    btns = {reset: "reset", inc: "inc", set: "set"};
 
     //при клике на set значение выбранного мин значения перебрасывается на screen
     onSet = () => {
@@ -20,48 +22,29 @@ class App extends React.Component {
         this.props.onIsButtonSet(true);
         this.props.onIsButtonInc(false);
         this.props.onIsButtonReset(false);
-
-        // this.props.onSetRedux(newMinValue)
-        // this.setState({
-        //   counter: newMinValue, isButtonInc: false, isButtonReset: false, isButtonSet: true
-        // }, () => {this.saveState()} )
     };
 
     //при достижении макс знач-я кнопка inc блокируется
- 
     onInc = () => {
-  
-       this.props.setIncrement(this.props.counter);
-     
-       if (this.props.counter === this.props.maxValue-1) {
- 
-        this.props.onIsButtonInc(true);
-        this.props.setNumberNewColor(true)
-
-      }
-    }
-     
-          
-
-       
-    
+        this.props.setIncrement(this.props.counter);
+        if (this.props.counter === this.props.maxValue - 1) {
+            this.props.onIsButtonInc(true);
+            this.props.setNumberNewColor(true)
+        }
+    };
 
     //сбрасывает при достижении макс значения
     onReset = () => {
         this.props.onIsButtonInc(false);
         this.props.setNumberNewColor(false);
-
-        let resetCounterValue = this.props.minValue
+        let resetCounterValue = this.props.minValue;
         this.props.onResetCounter(resetCounterValue)
     };
 
     //событие с мин и макс значениями
     onMinValue = (e) => {
-        let setMinValue = Number(e.target.value)
+        let setMinValue = Number(e.target.value);
         this.props.onMinValueRedux(setMinValue)
-        // this.setState({
-        //   minValue: Number(e.target.value)
-        // }, () => {this.saveState()});
     };
     onMaxValue = (e) => {
         let setMaxValue = Number(e.target.value);
@@ -75,21 +58,19 @@ class App extends React.Component {
         this.props.onIsButtonReset(true);
         this.props.setErrorMax(false);
         this.props.setErrorMin(false);
-        this.props.onSetEnteredValue()
+        this.props.onSetEnteredValue();
         if (this.props.maxValue <= -1) {
             this.props.onIsButtonSet(true);
             this.props.setErrorMax(true);
-            this.props.setErrorMin(false)
+            this.props.setErrorMin(false);
             this.props.onSetIncorrectValue()
         }
-        ;
         if (this.props.minValue <= -1) {
             this.props.onIsButtonSet(true);
             this.props.setErrorMax(false);
-            this.props.setErrorMin(true)
+            this.props.setErrorMin(true);
             this.props.onSetIncorrectValue()
         }
-        ;
         if (this.props.minValue >= this.props.maxValue) {
             this.props.onIsButtonSet(true);
             this.props.setErrorMax(true);
@@ -110,7 +91,6 @@ class App extends React.Component {
                                  onClickCounterValue={this.onClickCounterValue}
                                  errorMax={this.props.errorMax}
                                  errorMin={this.props.errorMin}
-                                 emptyValue={this.emptyValue}
                                  isButtonSet={this.props.isButtonSet}
                 />
                 <Counter counter={this.props.counter}
@@ -146,59 +126,59 @@ let mapStateToProps = (state) => {
 let mapDispatchToProps = (dispatch) => {
     return {
         onIsButtonSet(isButtonSet) {
-            const action = onIsButtonSetAC(isButtonSet)
+            const action = onIsButtonSetAC(isButtonSet);
             dispatch(action)
         },
         onIsButtonInc(isButtonInc) {
-            const action = onIsButtonIncAC(isButtonInc)
+            const action = onIsButtonIncAC(isButtonInc);
             dispatch(action)
         },
         onIsButtonReset(isButtonReset) {
-            const action = onIsButtonResetAC(isButtonReset)
+            const action = onIsButtonResetAC(isButtonReset);
             dispatch(action)
         },
         onSetRedux(newMinValue) {
-            const action = onSetReduxAC(newMinValue)
+            const action = onSetReduxAC(newMinValue);
             dispatch(action)
         },
         onMinValueRedux(setMinValue) {
-            const action = onMinValueReduxAC(setMinValue)
+            const action = onMinValueReduxAC(setMinValue);
             dispatch(action)
         },
         onMaxValueRedux(setMaxValue) {
-            const action = onMaxValueReduxAC(setMaxValue)
+            const action = onMaxValueReduxAC(setMaxValue);
             dispatch(action)
         },
         setNumberNewColor(numberNewColor) {
-            const action = setNumberNewColorAC(numberNewColor)
+            const action = setNumberNewColorAC(numberNewColor);
             dispatch(action)
         },
         setErrorMax(errorMax) {
-            const action = setErrorMaxAC(errorMax)
+            const action = setErrorMaxAC(errorMax);
             dispatch(action)
         },
         setErrorMin(errorMin) {
-            const action = setErrorMinAC(errorMin)
+            const action = setErrorMinAC(errorMin);
             dispatch(action)
         },
         setIncrement(counter) {
-            const action = setIncrementAC(counter)
+            const action = setIncrementAC(counter);
             dispatch(action)
         },
         onResetCounter(resetCounterValue) {
-            const action = onResetCounterAC(resetCounterValue)
+            const action = onResetCounterAC(resetCounterValue);
             dispatch(action)
         },
         onSetIncorrectValue() {
-            const action = onSetIncorrectValueAC()
+            const action = onSetIncorrectValueAC();
             dispatch(action)
         },
         onSetEnteredValue() {
-            const action = onSetEnteredValueAC()
+            const action = onSetEnteredValueAC();
             dispatch(action)
         },
     }
 };
 
-const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App)
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
 export default ConnectedApp
--- src/Counter.js
+++ src/Counter.js
@@ -4,28 +4,28 @@ import Screen from './Screen';
 import BtnsBlock from './BtnsBlock';
 
 
-
 class Counter extends React.Component {
 
-  render = () => {
-    return (
-      <div className='wrapperCounter'>
-        <Screen counter={this.props.counter}
-          numberNewColor={this.props.numberNewColor}
-          errorMax={this.props.errorMax}
-          errorMin={this.props.errorMin}
-        />
-        <BtnsBlock reset={this.props.reset}
-          inc={this.props.inc}
-          onInc={this.props.onInc}
-          onReset={this.props.onReset}
-          isButtonInc={this.props.isButtonInc}
-          counter={this.props.counter}
-          isButtonReset={this.props.isButtonReset}
-        />
-      </div>
-    );
-  }
+    render = () => {
+        return (
+            <div className='wrapperCounter'>
+                <Screen counter={this.props.counter}
+                        numberNewColor={this.props.numberNewColor}
+                        errorMax={this.props.errorMax}
+                        errorMin={this.props.errorMin}
+                />
+                <BtnsBlock reset={this.props.reset}
+                           inc={this.props.inc}
+                           onInc={this.props.onInc}
+                           onReset={this.props.onReset}
+                           isButtonInc={this.props.isButtonInc}
+                           counter={this.props.counter}
+                           isButtonReset={this.props.isButtonReset}
+                />
+            </div>
+        );
+    }
 }
+
 export default Counter;
 
